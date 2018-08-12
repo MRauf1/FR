a5641e569d4ba3fee7288233ec6185950a8bc680
Rauf <m_rauf@outlook.com>
2018-08-12 16:40:44 +0600

    Update data_augmentation.py

--- data_augmentation.py
+++ data_augmentation.py
@@ -1,5 +1,14 @@
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
-import sys
+from argparse import ArgumentParser
+from os.path import dirname
+
+# Command line arguments
+parser = ArgumentParser(description="Data augment a specific image 50 times")
+parser.add_argument("-p", "--path", required=True, help="Path to the image to be augmented.")
+
+# Get the initial variables 
+path = vars(parser.parse_args())["path"]
+directory = dirname(path)
 
 # Parameters/Options for data augmentation
 datagen = ImageDataGenerator(
@@ -12,14 +21,14 @@ datagen = ImageDataGenerator(
         fill_mode='nearest')
 
 # Retrieve the image
-image = load_img(sys.argv[2])
+image = load_img(path)
 X = img_to_array(image)
 X = X.reshape((1,) + X.shape)
 
 # Generate 50 new images and save them to the assigned directory
 i = 0
 for batch in datagen.flow(X, batch_size=1,
-                          save_to_dir=sys.argv[1], save_prefix='new', save_format='jpeg'):
+                          save_to_dir=directory, save_prefix='new', save_format='jpeg'):
     i += 1
     if i > 50:
         break
